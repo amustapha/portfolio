fd6517d968c30c35e474ea3e76785cfc3b996dd1
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-02-28 21:44:50 +0100

    Basic site

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 import 'spectre.css/dist/spectre.min.css'
-import 'spectre.css/dist/spectre-exp.min.css'
-import 'spectre.css/dist/spectre-icons.min.css'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
