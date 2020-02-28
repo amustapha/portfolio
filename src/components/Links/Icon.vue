8a2efbbb4cd5c7ca4f4297388f825eeccae04050
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-02-28 21:37:14 +0100

    Created components for adding icons with text

---
+++ src/components/Links/Icon.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="column col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-6 p-2 my-2">
+        <div class="container p-2 my-2">
+            <span :class="`icon ${icon}`"></span>
+            <span>{{text}}</span>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Icon",
+        props: ['icon', 'text']
+    }
+</script>
+
+<style scoped>
+    span {
+        display: block;
+        text-align: center;
+    }
+
+    .icon{
+        font-size:2.5em;
+    }
+</style>
---
+++ src/components/Links/index.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container bg-gray p-2">
+        <div class="columns p-2 m-2">
+            <icon :icon="tech.icon" :text="tech.text" v-for="tech in techs" :key="tech.icon"/>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import Icon from "./Icon";
+    export default {
+        name: "Links",
+        components: {Icon},
+        data () {
+            return {
+                techs: [
+                    {
+                        icon: 'icon-python',
+                        text: 'Python'
+                    },
+                    {
+                        icon: 'icon-django',
+                        text: 'Django'
+                    },
+                    {
+                        icon: 'icon-javascript',
+                        text: 'Javascript'
+                    },
+                    {
+                        icon: 'icon-flask',
+                        text: 'Flask'
+                    },
+                    {
+                        icon: 'icon-vue-dot-js',
+                        text: 'Vue JS'
+                    },
+                    {
+                        icon: 'icon-amazonaws',
+                        text: 'AWS'
+                    },
+                    {
+                        icon: 'icon-mongodb',
+                        text: 'MongoDB'
+                    },
+                    {
+                        icon: 'icon-googlecloud',
+                        text: 'GCP'
+                    },
+                    {
+                        icon: 'icon-java',
+                        text: 'Java'
+                    },
+                    {
+                        icon: 'icon-c',
+                        text: 'C/C++'
+                    },
+                    {
+                        icon: 'icon-docker',
+                        text: 'Docker'
+                    },
+                    {
+                        icon: 'icon-jira',
+                        text: 'Jira'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
