194e96d0f46b2d19ab9a958befaebaacdc807dec
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-02-28 21:37:40 +0100

    Top banner

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hero hero-lg bg-primary">
+        <div class="hero-body text-center px-2">
+            <h1 class="text-bold">Abdulhakeem Adetunji Mustapha</h1>
+            <p class="my-about">Creative and result-oriented Software Engineer with over 6 years of experience building amazing web and
+                mobile applications, coordinating cross-functional teams and easily communicating complex technical
+                details to non-technical stakeholders.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Hero"
+    }
+</script>
+
+<style scoped>
+    .my-about {
+        max-width: 680px;
+        margin: auto;
+    }
+
+</style>
