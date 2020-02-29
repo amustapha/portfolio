ecbb88b5605da5d4f017fa9e5afc59cd2a22db4b
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-02-29 14:18:41 +0100

    Fix spacing

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="hero hero-lg bg-primary  px-2">
         <div class="hero-body text-center px-2 mx-2">
-            <img src="my-pic-no-bg.png" class="me" height="460px" />
+            <img src="my-pic-no-bg.png" class="me" height="460px"/>
             <h1 class="text-bold front">Abdulhakeem Adetunji Mustapha</h1>
-            <p class="my-about front">Creative and result-oriented Software Engineer with over 6 years of experience building amazing web and
+            <p class="my-about front mb-2 pb-2">Creative and result-oriented Software Engineer with over 6 years of
+                experience building amazing web and
                 mobile applications, coordinating cross-functional teams and easily communicating complex technical
                 details to non-technical stakeholders.</p>
+            <a href="https://docs.google.com/document/d/1xElMvl-O5xN6oA5U989hwJlEwEseLTA-QxpNFkxs73Q/edit?usp=sharing"
+               target="_blank"
+               class="btn mt-2">Checkout my resume</a>
         </div>
     </div>
 </template>
@@ -33,7 +37,7 @@
         z-index: 20;
     }
 
-    .hero{
+    .hero {
         position: relative;
     }
 
