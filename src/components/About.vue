76f787d80a21384867c679eaf9557d4b8d91dbfc
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-02-29 15:34:39 +0100

    Added contact information

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="hero hero-sm bg-dark">
-        <div class=" container container-fluid text-center">
-            <h4 class="text-bold text-uppercase">about this page</h4>
-        </div>
-        <div class=" container container-fluid text-center">
-            <div class="columns">
-                <div class="column">
-                    Can you believe this page is under 256KB excluding the font-icons.
+        <div class="container-fluid py-2 my-2">
+
+            <div class=" container text-center">
+                <h4 class="text-bold text-uppercase">about this page</h4>
+            </div>
+            <div class=" container  text-center mt">
+                <div class="columns">
+                    <div class="column">
+                        Can you believe this page is under 256KB excluding the font-icons.
+                    </div>
+                    <div class=" divider-vert text-center"/>
+                    <div class="column">
+                        This page is built with <a href="https://vuejs.org" target="_blank">VueJS</a> and <a
+                            href="https://picturepan2.github.io/spectre/" target="_blank">Spectre.css</a>
+                    </div>
                 </div>
-                <div class=" divider-vert text-center" />
-                <div class="column">
-                    This page is built with <a href="https://vuejs.org">VueJS</a> and <a href="https://picturepan2.github.io/spectre/">Spectre.css</a>
+                <div class="columns p-2 m-2">
+                    <div class="column p-2 m-2 text-bold text-large ">
+                        If you need a scalable and light web application, I'm your guy. Send me an email at <a href="mailto:abdulhakeemmustapha@gmail.com" class="btn">abdulhakeemmustapha@gmail.com</a> .
+                    </div>
+                </div>
+
+                <div class="columns">
+                    <div class="column">
+                        Need help? Shoot me an email on <a href="mailto:abdulhakeemmustapha@gmail.com" class="btn">abdulhakeemmustapha@gmail.com</a> or give me a call
+                        <a href="tel:+2347067577058" class="btn btn-primary">+234 706 757 7058</a> .
+                    </div>
                 </div>
             </div>
         </div>
@@ -24,5 +40,6 @@
 </script>
 
 <style scoped>
+
 
 </style>
